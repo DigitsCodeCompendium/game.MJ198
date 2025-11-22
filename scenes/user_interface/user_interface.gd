5c526566c9e4cdbf613dc09a35e98f12411456d2
Antoine Burnt <antoine.brule@hotmail.ca>
2025-11-21 22:50:50 -0600

    Added weapon/engine pickup UI

--- scenes/user_interface/user_interface.gd
+++ scenes/user_interface/user_interface.gd
@@ -3,25 +3,39 @@ extends Control
 @onready var weapon_icon = get_node("%Weapon")
 @onready var engine_icon = get_node("%Engine")
 @onready var free_power_bar = get_node("%ReactorPower")
+
 @onready var speed_label = get_node("%SpeedLabel")
 @onready var speed_bar = get_node("%SpeedProgressBar")
+
 @onready var pending_module = get_node("%PickedUpModule")
 @onready var pending_module_timer = get_node("%ModuleTimer")
 @onready var pending_module_bar = get_node("%ModuleDiscardProgressBar")
 
+@onready var pending_wep_eng = get_node("%PickedUpEngWep")
+@onready var pending_wep_eng_timer = get_node("%EngWepTimer")
+@onready var pending_wep_eng_bar = get_node("%EngWepDiscardProgressBar")
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	UiEventBus.connect("reactor_updated",_on_power_changed)
+	
 	UiEventBus.connect("current_speed",_on_speed_update)
+	
 	UiEventBus.connect("module_pending_added",_on_module_added)
 	UiEventBus.connect("module_pending_lost",_on_module_lost)
 	UiEventBus.connect("module_pending_discarded",_on_module_lost)
+	
+	UiEventBus.connect("weapon_pending_added",_on_weapon_added)
+	UiEventBus.connect("weapon_pending_discarded",_on_weapon_lost)
+	UiEventBus.connect("weapon_pending_lost",_on_weapon_lost)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if not pending_module_timer.is_stopped():
 		pending_module_bar.value = pending_module_timer.get_time_left()
-		
+	
+	if not pending_wep_eng_timer.is_stopped():
+		pending_wep_eng_bar.value = pending_wep_eng_timer.get_time_left()
 
 func _on_power_changed(curent_max_power:int,free_power:int):
 	free_power_bar.value = free_power
@@ -34,7 +48,13 @@ func _on_module_added(module:BaseModule):
 	pending_module.visible = true
 	pending_module_timer.start(5)
 	
-	
 func _on_module_lost():
 	pending_module.visible = false
 	pending_module_timer.stop()
+
+func _on_weapon_added(weapon: Shootable):
+	pending_wep_eng.visible = true
+	pending_wep_eng_timer.start(5)
+func _on_weapon_lost():
+	pending_wep_eng.visible = false
+	pending_wep_eng_timer.stop()
