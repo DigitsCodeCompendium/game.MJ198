9d70f900ac0984046770cd42ab23f9b73bdc9ec1
syy1125 <syy1125@hotmail.com>
2025-11-22 16:35:53 -0800

    Player can customize engine

--- scenes/title_screen/title_screen.gd
+++ scenes/title_screen/title_screen.gd
@@ -4,12 +4,10 @@ extends Control
 @export var main_menu: Control
 @export var options_menu: Control
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	main_menu.visible = true
 	options_menu.visible = false
-
 
 func _start_game():
 	get_tree().change_scene_to_packed(game_scene)
