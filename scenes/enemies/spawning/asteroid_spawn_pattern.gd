37e455a1915d400833cb1b62ad6e567806f52c65
Antoine Burnt <antoine.brule@hotmail.ca>
2025-11-23 15:03:38 -0600

    Added asteroids variants to asteroid spawn pattern

--- scenes/enemies/spawning/asteroid_spawn_pattern.gd
+++ scenes/enemies/spawning/asteroid_spawn_pattern.gd
@@ -1,14 +1,14 @@
 extends BaseEnemySpawnPattern
 class_name AsteroidSpawnPattern
 
-@export var asteroid_scene: PackedScene
+@export var asteroid_scenes: Array[PackedScene]
 @export var horizontal_speed_range: float = 10
 @export var min_vertical_speed: float = 100
 @export var max_vertical_speed: float = 200
 @export var linear_speed_scale: float = 1
 
 func do_spawn(parent: Node2D, position: Vector2, rand: RandomNumberGenerator):
-	var asteroid = asteroid_scene.instantiate()
+	var asteroid = asteroid_scenes.pick_random().instantiate()
 	var speed_control: SpeedControl = parent.get_node("/root/Game/SpeedControl")
 	
 	var velocity_x = rand.randf_range(-horizontal_speed_range, horizontal_speed_range)
