ea7ab98e05233d11968cfdf0230514add95186e2
syy1125 <syy1125@hotmail.com>
2025-11-23 15:21:20 -0800

    Fix exploder ship AI and direction

--- scenes/systems/enemies/ai/charge_ai.gd
+++ scenes/systems/enemies/ai/charge_ai.gd
@@ -12,8 +12,8 @@ func _process(delta):
 	owner.position += _velocity * delta
 	
 	var target_dir: Vector2 = player.global_position - owner.global_position
-	var rot_diff = angle_difference(area.rotation, target_dir.angle()) + PI/2
+	var rot_diff = angle_difference(area.rotation + PI / 2, target_dir.angle())
 	var rotation_limit = rotation_speed * delta * PI / 180
 	area.rotate(clampf(rot_diff, -rotation_limit, rotation_limit))
 	
-	_velocity += area.transform.x.normalized() * acceleration * delta
+	_velocity += area.transform.y.normalized() * acceleration * delta
