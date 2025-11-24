eeaace32924bbbe69cb33b341a2716a04578c2b1
Antoine Burnt <antoine.brule@hotmail.ca>
2025-11-23 21:07:43 -0600

    Added moving background starts

--- scenes/background/background.gd
+++ scenes/background/background.gd
@@ -1,9 +1,8 @@
 extends Node2D
 
-@export
-var scrolling_backgrounds: Array[Parallax2D]
-
-var scroll_speed: float = 1
+@export var scrolling_backgrounds: Array[Parallax2D]
+@onready var speed_control = get_parent().get_node("SpeedControl")
+var scroll_speed = Vector2()
 
 func set_scroll_speed(speed:float):
 	scroll_speed = speed
@@ -15,5 +14,6 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	for background in scrolling_backgrounds:
-		background.scroll_offset.y += scroll_speed * background.scroll_scale.y
+	scroll_speed.y = speed_control.relativistic_speed * 250
+	$Parallax2D2.autoscroll = scroll_speed
+	pass
