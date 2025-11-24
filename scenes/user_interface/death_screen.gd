8acfad0ed753a59e7b098281154f1150da16cf6c
syy1125 <syy1125@hotmail.com>
2025-11-23 16:45:38 -0800

    Add mini movie at death

--- scenes/user_interface/death_screen.gd
+++ scenes/user_interface/death_screen.gd
@@ -3,6 +3,8 @@ extends Control
 @export_file var main_menu_scene: String
 @export_file var game_scene: String
 
+@export var death_animation: AnimatedSprite2D
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	UiEventBus.connect("player_died", _on_player_died)
@@ -10,6 +12,11 @@ func _ready():
 func _on_player_died():
 	visible = true
 	get_tree().paused = true
+	
+	death_animation.play()
+	await death_animation.animation_finished
+	$PanelContainer/DeathMenuContainer.visible = true
+	$PanelContainer/AnimationContainer.visible = false
 
 func _restart_game():
 	get_tree().paused = false
