cf3d233beeec019cb45ba2169aa5a46e6fca7194
syy1125 <syy1125@hotmail.com>
2025-11-23 17:28:21 -0800

    Remove debug keybind

--- scenes/game.gd
+++ scenes/game.gd
@@ -2,14 +2,15 @@ extends Node2D
 
 var asteroid_scene = preload("res://scenes/enemies/asteroids/asteroid.tscn")
 var playable_margins: float
-@export var spawn_timer = 0.5 #spawn timer of asteroids in seconds
-var spawn_timer_time = 0
+#@export var spawn_timer = 0.5 #spawn timer of asteroids in seconds
+#var spawn_timer_time = 0
 
 func _ready() -> void:
 	playable_margins = 0.125*get_viewport().size.x
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
+	pass
 	#spawn_timer_time += delta
 	#
 	#if spawn_timer_time > spawn_timer:
@@ -20,5 +21,5 @@ func _process(_delta: float) -> void:
 		#add_child(asteroid)
 		#asteroid.launch(Vector2(randi_range(-10, 10), 200), Vector2(randi_range(playable_margins, screen_size.x-playable_margins),-20), randf_range(1, 5))
 	
-	if Input.is_action_just_pressed("test_key"):
-		get_node("Player/Systems/ModuleSystem").set_module(0, load("res://resources/modules/firerate_module_1.tres"))
+	#if Input.is_action_just_pressed("test_key"):
+		#get_node("Player/Systems/ModuleSystem").set_module(0, load("res://resources/modules/firerate_module_1.tres"))
