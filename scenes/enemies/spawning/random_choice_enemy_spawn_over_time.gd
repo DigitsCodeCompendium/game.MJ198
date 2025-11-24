41583cbc79c8595412113732cd58db541e30524e
syy1125 <syy1125@hotmail.com>
2025-11-23 18:22:04 -0800

    Adjust enemy spawn pattern over time

--- scenes/enemies/spawning/random_choice_enemy_spawn_over_time.gd
+++ scenes/enemies/spawning/random_choice_enemy_spawn_over_time.gd
@@ -2,11 +2,13 @@ extends BaseEnemySpawn
 class_name RandomEnemySpawnOverTime
 
 @export var spawn_table: Dictionary[PackedScene, Curve] = {}
+@export var use_linear_speed: bool
 
 func instantiate_enemy(rand: RandomNumberGenerator, params: EnemySpawnParams):
 	assert(len(spawn_table) > 0)
 	var enemies = spawn_table.keys()
-	var weights = enemies.map(func(e): return spawn_table[e].sample(params.relativistic_speed))
+	var speed = params.linear_speed if use_linear_speed else params.relativistic_speed
+	var weights = enemies.map(func(e): return spawn_table[e].sample(speed))
 	var enemy_scene = enemies[rand.rand_weighted(weights)]
 	
 	if enemy_scene == null:
